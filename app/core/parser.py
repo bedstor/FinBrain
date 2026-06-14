efb3b0d8e152d16651f1d95f30fd5a3371cbed86
bedstor <ivanchikhal12@gmail.com>
2026-06-14 21:19:12 +0700

    Класс TBankParser был улучшен

--- app/core/parser.py
+++ app/core/parser.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import re
 
 class BaseBankParser:
     """Стандартная модель банка"""
@@ -29,12 +30,22 @@ class TBankParser(BaseBankParser):
     """Модель Т-банка"""
 
     def _process_text(self, text: str) -> list:
-        """Разрезаем текст на строки и выводим"""
+        """
+        Разрезаем текст на строки
+        """
         lines = text.split("\n")
+        pattern = r"(?P<date>^\d{2}\.\d{2}\.\d{4})\s(?P<category>[А-Яа-яA-Za-z]+)\s"
+        pattern += r"(?P<sum>[+-]?\d+\.\d{2})\s(?P<description>\D[А-Яа-яA-Za-z]+$)"
+        result = []
         for line in lines:
-            print(line)
-
-        return []
+            match = re.match(pattern, line)
+            if not match:
+                continue # Если None - пропускаем строку
+            transaction_dict = match.groupdict()
+            result.append(transaction_dict)
+                
+        return result
+            
 
 parser = TBankParser("data/test_bank.pdf")
 print(parser.parse())
